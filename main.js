9826b39904c4a9495222d9e4abde0cd15a7f6398
jurkiewicz <jzdzisio@wp.pl>
2017-09-08 17:20:33 +0200

    Moving object, tracking mouse, transition styles

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", function(){
 	el.id = 'jack';
 	el.style.width = '100px';
 	el.style.height = '100px';
-	el.style.background = '#FF6633';
 
 	var body = document.getElementsByTagName('body')[0];
 	body.appendChild(el);
@@ -24,10 +23,29 @@ document.addEventListener("DOMContentLoaded", function(){
 	    return {pozX,pozY}
 	}
 
+	//Tracking mouse by object
+	function moveJack(e){
+		var jack = document.getElementById('jack');
+		var pozX = 0;
+	    var pozY = 0;
+	    if (!e) var e = window.event;
+	    if (e.pageX || e.pageY) {
+	         pozX = e.pageX;
+	         pozY = e.pageY;
+	    } else if (e.clientX || e.clientY) {
+	         pozX = e.clientX + document.body.scrollLeft;
+	         pozY = e.clientY + document.body.scrollTop;
+	    }
+	    with (jack.style) {
+	    	left = pozX-55+'px';
+	    	top = pozY-55+'px';
+	    }
+	    return {pozX,pozY}
+	}
+
 	//Telling about mouse position
 	document.getElementById('Body').addEventListener('mousemove', function(e) {
     	console.log(mousePosition(e));
+    	moveJack(e);
 	});
-
-
 });
